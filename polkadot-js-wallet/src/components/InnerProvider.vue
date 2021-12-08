c30d458f8bd16e9d723c902cce4d93b7f4035b26
Simono il magnifico <simoneromano@protonmail.ch>
2021-12-08 15:20:25 +0100

    send tx, balance

--- polkadot-js-wallet/src/components/InnerProvider.vue
+++ polkadot-js-wallet/src/components/InnerProvider.vue
@@ -1,53 +1,75 @@
 <script setup lang="ts">
-import { reactive, ref } from "vue";
+import { onBeforeUnmount, reactive, ref } from "vue";
 
 import { ApiPromise, WsProvider } from "@polkadot/api";
-import { web3Accounts, web3Enable, web3FromAddress, web3ListRpcProviders, web3UseRpcProvider } from "@polkadot/extension-dapp";
+import { web3Accounts, web3AccountsSubscribe, web3Enable, web3FromAddress, web3ListRpcProviders, web3UseRpcProvider } from "@polkadot/extension-dapp";
 
-import type { InjectedAccountWithMeta } from "@polkadot/extension-inject/types";
+import type { InjectedAccountWithMeta, Unsubcall } from "@polkadot/extension-inject/types";
 
-const RPC_URL = "wss://westend-rpc.polkadot.io";
+const RPC_URL = "";
 
 const wsProvider = new WsProvider(RPC_URL);
 const api = await ApiPromise.create({ provider: wsProvider });
 
 // returns an array of all the injected sources
 // (this needs to be called first, before other requests)
+let unsubscribe: Unsubcall | undefined;
+const accounts = ref<InjectedAccountWithMeta[]>([]);
 const enable = async () => {
   try {
-    const allInjected = await web3Enable("my cool dapp");
-    console.log(allInjected);
-  } catch (error) {}
+    await web3Enable("my cool dapp");
+    // we subscribe to any account change and log the new list.
+    // note that `web3AccountsSubscribe` returns the function to unsubscribe
+    const allAccounts = await web3Accounts();
+    accounts.value = allAccounts;
+
+    unsubscribe = await web3AccountsSubscribe((injectedAccounts) => {
+      accounts.value = injectedAccounts;
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-const accounts = ref<InjectedAccountWithMeta[]>([]);
-const getAccounts = async () => {
-  // meta.source contains the name of the extension that provides this account
-  const allAccounts = await web3Accounts();
-  accounts.value = allAccounts;
+const signAndSendTransaction = async () => {
+  try {
+    const currentAccounts = accounts.value;
+    const sender = currentAccounts.at(0);
+    if (!sender) {
+      throw new Error("Missing sender address");
+    }
+    const destination = currentAccounts.at(1);
+    if (!destination) {
+      throw new Error("Missing destination address");
+    }
+    const injector = await web3FromAddress(sender.address);
+
+    const signed = await api.tx.balances.transfer(destination.address, 0.1).signAndSend(sender.address, { signer: injector.signer });
+    console.log("🚀 ~ file: InnerProvider.vue ~ line 45 ~ signMessage ~ signed", signed);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-const signTransaction = async () => {
-  //   // finds an injector for an address
-
+const getBalance = async () => {
   const currentAccounts = accounts.value;
   const sender = currentAccounts.at(0);
   if (!sender) {
     throw new Error("Missing sender address");
   }
-  const destination = currentAccounts.at(1);
-  if (!destination) {
-    throw new Error("Missing destination address");
-  }
-  const injector = await web3FromAddress(sender.address);
-
-  const signed = await api.tx.balances.transfer(destination.address, 0.1).signAsync(sender.address, { signer: injector.signer });
-  console.log("🚀 ~ file: InnerProvider.vue ~ line 45 ~ signMessage ~ signed", signed);
+  const {data} = await api.query.system.account(sender.address)
+  console.log("🚀 ~ file: InnerProvider.vue ~ line 61 ~ getBalance ~ balance", data.free.toNumber())
 };
+
+onBeforeUnmount(() => {
+  // console.log("🚀 ~ file: InnerProvider.vue ~ line 49 ~ onBeforeUnmount ~ onBeforeUnmount")
+  unsubscribe?.();
+});
 </script>
 
 <template>
   <button @click="enable">Connect Polkadot JS</button>
-  <button @click="getAccounts">Get my accounts</button>
-  <button @click="signTransaction">Sign transaction</button>
+  <!-- <button @click="getAccounts">Get my accounts</button> -->
+  <button @click="signAndSendTransaction">Sign and send transaction</button>
+  <button @click="getBalance">Get balance</button>
 </template>
